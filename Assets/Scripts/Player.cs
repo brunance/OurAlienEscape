849079f8b49613b14c51c166906ea8dc492afa86
Bruno <bruno_french@outlook.com>
2022-08-06 17:21:01 -0400

    create and finish player animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,11 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    private Animator anim;
+
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,6 +22,24 @@ public class Player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * speed;
+
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("run", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("run", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        if (Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("run", false);
+        }
+
     }
 
 }
