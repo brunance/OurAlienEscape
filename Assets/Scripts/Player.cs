80889735f5abc1632cf075f011d7949d9b652fb2
Bruno <bruno_french@outlook.com>
2022-08-06 16:46:23 -0400

    player movement finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+    }
+
 }
